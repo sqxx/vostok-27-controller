7b6e44ebe3a1bc78e3faf17d657f2a700af60886
Valery Klachkov <sqxx.kv@gmail.com>
2019-10-27 19:17:55 +1000

    Mega improvements

--- OnBoardController/OnBoardController.ino
+++ OnBoardController/OnBoardController.ino
@@ -1,13 +1,3 @@
-/*
-   Программа для управления станцией Восток-27
-
-   Автор: Клачков sqxx Валерий
-   Дата: хх.хх.2019
-
-   Для обмена данными с внешним миром используется специфичный протокол,
-     описанный в сопроводительном документе
-*/
-
 /* -- Библиотеки --- */
 
 #include <Wire.h>
@@ -28,26 +18,32 @@
 */
 #define FLOAT_TO_INT(x) (((int) x) + ((((int)(x * 100)) % 100) >= 50 ? 1 : 0))
 
-#define IN_RANGE(x, l, h) (x >= l && x <= h)
+#define IS_IN_RANGE(x, l, h) (x >= l && x <= h)
+
+#define RUN(pin)  (digitalWrite(pin, LOW))
+#define STOP(pin) (digitalWrite(pin, HIGH))
+
+#define NOTIFY(cmd) (send_package(cmd, 0))
 
 /* -- НАСТРОЙКИ --- */
 
 #define MQ135_PIN A0
 #define MQ135_CALLIBRATION_DATA 77
 
-#define DHT_PIN  4
+#define DHT_PIN  3
 #define DHT_TYPE DHT11
 
 #define PIN_SOLAR_PANEL_LEFT  A1
 #define PIN_SOLAR_PANEL_RIGHT A2
 
-#define PIN_PROD_CO2          5
-#define PIN_OXYGEN_SUPPLY     13
-#define PIN_PRES_RELIEF_VALVE 10
+#define PIN_PROD_CO2   31
+#define PIN_PUMP_VALVE 32
+#define PIN_PRES_RELIEF_VALVE 33
 
-#define PIN_LIGHT             0
+#define PIN_LIGHT 34
 
-#define _MA_STATE  0x00
+// MA - Memory Address
+#define _MA_STATE 0x00
 #define _MA_STATE_INITIALIZED 0xAABBCCDD
 #define _MA_DAY_TIME   (_MA_STATE + sizeof(uint32_t))
 #define _MA_NIGHT_TIME (_MA_DAY_TIME + sizeof(uint32_t))
@@ -55,14 +51,16 @@
 
 /* -- ПРОТОКОЛ --- */
 
-#define PACKAGE_SIZE 8
-#define MAGIC_BYTE   0xF4
+#define PACKAGE_SIZE 10
+#define PACKAGE uint8_t package[]
+
+#define START_MAGIC 0xF4
+#define END_CR 0x0A
+#define END_LF 0x0D
 
 #define _P_STARTUP       0x01
 #define _P_INIT_COMPLETE 0x02
 #define _P_NOT_READY     0x03
-#define _P_PRESSURE_LOSS 0x04
-#define _P_UNKNOWN_CMD   0x0F
 
 #define _P_REQ_CO2  0xA1  // CO2 (ppm)
 #define _P_REQ_HUM  0xA2  // Влажность (%)
@@ -80,10 +78,10 @@
 
 #define _P_SWITCH_PRES_RELIEF_VALVE  0xB1  // Переключить клапан сброса давления
 #define _P_STATUS_PRES_RELIEF_VALVE  0xB2  // Состояние клапана сброса давления
-#define _P_SWITCH_OXYGEN_SUPPLY  0xB3  // Переключить систему подачи кислорода
-#define _P_STATUS_OXYGEN_SUPPLY  0xB4  // Состояние системы подачи кислорода  
-#define _P_SWITCH_PROD_CO2  0xB5  // Переключить выработку углекислого газа
-#define _P_SWITCH_PROD_CO2  0xB6  // Состояние системы выработки углекислого газ
+#define _P_SWITCH_PUMP_VALVE 0xB3  // Переключить систему подачи кислорода
+#define _P_STATUS_PUMP_VALVE 0xB4  // Состояние системы подачи кислорода  
+#define _P_SWITCH_PROD_CO2   0xB5  // Переключить выработку углекислого газа
+#define _P_STATUS_PROD_CO2   0xB6  // Состояние системы выработки углекислого газ
 
 #define _P_SET_TIME       0xD1
 #define _P_SET_DAY_TIME   0xD2
@@ -92,20 +90,33 @@
 #define _P_GET_DAY_TIME   0xD5
 #define _P_GET_NIGHT_TIME 0xD6
 
+#define _P_SERIAL_RESET   0xEA  // Отчистка всего буфера Serial
+
+// PE - Protocol Exception
+#define _PE_GAS_LEAK    0xE1  // Утечка газа
+#define _PE_PACKAGE_ERR 0xE3  // Неверный пакет
+#define _PE_PACKAGE_CRC 0xE4  // Неверная контрольная сумма
+#define _PE_UNKNOWN_CMD 0xE5  // Неизвестная команда
+
+#define _PE_PACKAGE_ERR_CRLF  0xDA
+#define _PE_PACKAGE_ERR_MAGIC 0xF4
+
 
 /* -- ОБЪЕКТЫ --- */
 
 MQ135 mq135(MQ135_PIN);
-DHT dht(DHT_PIN, DHT_TYPE);
+DHT   dht(DHT_PIN, DHT_TYPE);
 Barometer bar;
 
-bool prod_co2_active = false;
-bool oxygen_supply_active = false;
 bool pres_relief_valve_active = false;
+bool pump_valve_active   = false;
+bool prod_co2_active = false;
 
-uint32_t day_timestamp = 0;
+uint32_t day_timestamp   = 0;
 uint32_t night_timestamp = 0;
+
 bool light_active = false;
+
 
 /* -- MAIN & LOOP --- */
 
@@ -113,15 +124,15 @@ void setup()
 {
   Serial.begin(9600);
 
-  send_package(_P_STARTUP, 0);
+  NOTIFY(_P_STARTUP);
 
-  pinMode(PIN_PROD_CO2, OUTPUT);
-  pinMode(PIN_OXYGEN_SUPPLY, OUTPUT);
+  pinMode(PIN_PROD_CO2,   OUTPUT);
+  pinMode(PIN_PUMP_VALVE, OUTPUT);
   pinMode(PIN_PRES_RELIEF_VALVE, OUTPUT);
 
-  digitalWrite(PIN_PROD_CO2, HIGH);
-  digitalWrite(PIN_OXYGEN_SUPPLY, HIGH);
-  digitalWrite(PIN_PRES_RELIEF_VALVE, HIGH);
+  STOP(PIN_PROD_CO2);
+  STOP(PIN_PUMP_VALVE);
+  STOP(PIN_PRES_RELIEF_VALVE);
 
   mq135.heaterPwrHigh();
   dht.begin();
@@ -130,51 +141,49 @@ void setup()
   init_eeprom();
   init_time();
 
-  send_package(_P_INIT_COMPLETE, 0);
+  NOTIFY(_P_INIT_COMPLETE);
 }
 
 void loop()
 {
-  handle_time();
+  uint8_t  package[PACKAGE_SIZE];
+  uint16_t package_crc = 0;
+  uint16_t calc_crc    = 0;
 
-  uint8_t package[PACKAGE_SIZE];
+  // Управление освещением
+  handle_light();
 
-  uint16_t given_crc = 0;
-  uint16_t calc_crc = 0;
-
-  // Ожидаем, пока прогреется датчик и калибруем
-  if (mq135.heatingCompleted() && !mq135.isCalibrated())
+  // Завершение настройки датчиков
+  if (!setup_mq135())
   {
-    send_package(_P_NOT_READY, 0);
-
-    mq135.calibrate(MQ135_CALLIBRATION_DATA);
-
+    NOTIFY(_P_NOT_READY);
     return;
   }
 
   // Проверяем доступность данных
-  if (Serial.available() < PACKAGE_SIZE)
-  {
-    return;
-  }
+  if (Serial.available() < PACKAGE_SIZE) return;
 
   // Читаем пакет
   memset(package, 0, PACKAGE_SIZE);
   Serial.readBytes(package, PACKAGE_SIZE);
 
-  if (package[0] != MAGIC_BYTE)
+  // Проверка маркеров пакета (см. документацию)
+  if (package[0] !=  START_MAGIC ||
+      package[PACKAGE_SIZE - 2] != END_CR ||
+      package[PACKAGE_SIZE - 1] != END_LF)
   {
-    Serial.println("Magic byte error.");
+    handle_bad_package(package);
     return;
   }
 
-  given_crc = (((unsigned int)package[PACKAGE_SIZE - 1]) << 8) | package[PACKAGE_SIZE - 2];
-  calc_crc = calculate_crc(package);
-
   // Проверяем контрольную сумму
-  if (given_crc != calc_crc)
+  package_crc = extract_crc(package);
+  calc_crc    = calculate_crc(package);
+
+  if (package_crc != calc_crc)
   {
-    Serial.println("CRC error. Give " + String(given_crc) + ", expected " + String(calc_crc));
+    Serial.println("CRC: " + String(package_crc, HEX));
+    handle_bad_crc(package, package_crc);
     return;
   }
 
@@ -183,8 +192,61 @@ void loop()
 }
 
 
+/* -- ЖЕЛЕЗО -- */
+
+/*
+   Запускает прогрев MQ-135
+
+   Возвращает true, если прогрев уже прошёл
+*/
+bool setup_mq135()
+{
+  if (mq135.heatingCompleted() && !mq135.isCalibrated())
+  {
+    mq135.calibrate(MQ135_CALLIBRATION_DATA);
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+/*
+   Управление освещением в зависимости от времени
+*/
+void handle_light()
+{
+  tmElements_t tm;
+  RTC.read(tm);
+
+  int ts = (tm.Hour * 60 * 60) + (tm.Minute * 60) + tm.Second;
+
+  if (ts >= day_timestamp &&
+      ts < night_timestamp)
+  {
+    if (light_active)
+    {
+      digitalWrite(PIN_LIGHT, LOW);
+      light_active = false;
+    }
+  }
+  else
+  {
+    if (!light_active)
+    {
+      digitalWrite(PIN_LIGHT, HIGH);
+      light_active = true;
+    }
+  }
+}
+
+
 /* -- ЛОГИКА --- */
 
+/*
+   Подгружает настройки из EEPROM памяти или инициализирует её
+*/
 void init_eeprom()
 {
   uint32_t crc = 0x00;
@@ -204,6 +266,9 @@ void init_eeprom()
   }
 }
 
+/*
+   Синхронизирует rtc модуль со временем на ПК
+*/
 void init_time()
 {
   tmElements_t tm;
@@ -223,54 +288,49 @@ void init_time()
   }
 }
 
-void handle_time()
+/*
+   Отчищает весь буфер Serial
+*/
+void serial_reset()
 {
-  tmElements_t tm;
-  RTC.read(tm);
-
-  int ts = (tm.Hour * 60 * 60) + (tm.Minute * 60) + tm.Second;
-
-  if (day_timestamp <= ts && ts <= night_timestamp)
+  while (Serial.available())
   {
-    if (light_active)
-    {
-      digitalWrite(PIN_LIGHT, LOW);
-      light_active = false;
-    }
-  }
-  else
-  {
-    if (!light_active)
-    {
-      digitalWrite(PIN_LIGHT, HIGH);
-      light_active = true;
-    }
+    Serial.read();
   }
 }
 
+
+/* -- ПРОТОКОЛ --- */
+
+/*
+   Формирует и отправляет пакет по Serial
+*/
 void send_package(uint8_t cmd, uint32_t value)
 {
-  uint8_t package[PACKAGE_SIZE + 2] = {MAGIC_BYTE, cmd, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x0D};
+  uint8_t  package[PACKAGE_SIZE] = {START_MAGIC, cmd, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x0D};
   uint32_t crc = 0;
 
-  package[2] = (value & 0x000000FF);
-  package[3] = (value & 0x0000FF00) >> 8;
-  package[4] = (value & 0x00FF0000) >> 16;
-  package[5] = (value & 0xFF000000) >> 24;
+  package[2] = (value & 0xFF000000) >> 24;
+  package[3] = (value & 0x00FF0000) >> 16;
+  package[4] = (value & 0x0000FF00) >> 8;
+  package[5] = (value & 0x000000FF);
 
   crc = calculate_crc(package);
 
-  package[6] = (crc & 0x00FF);
-  package[7] = (crc & 0xFF00) >> 8;
+  package[6] = (crc & 0xFF00) >> 8;
+  package[7] = (crc & 0x00FF);
 
-  Serial.write(package, PACKAGE_SIZE + 2);
+  Serial.write(package, PACKAGE_SIZE);
 }
 
-uint32_t calculate_crc(uint8_t package[])
+/*
+   Подсчитывает контрольную сумму для переданного пакета
+*/
+uint32_t calculate_crc(PACKAGE)
 {
   uint32_t crc = 0;
 
-  for (uint32_t i = 1; i < (PACKAGE_SIZE - 2); i++)
+  for (uint32_t i = 1; i < (PACKAGE_SIZE - 4); i++)
   {
     crc += package[i];
   }
@@ -278,29 +338,71 @@ uint32_t calculate_crc(uint8_t package[])
   return crc;
 }
 
-void handle_request(uint8_t package[])
+/*
+   Извлекает контрольную сумму из переданного пакета
+*/
+uint16_t extract_crc(PACKAGE)
+{
+  byte high_byte = (uint16_t) package[PACKAGE_SIZE - 4];
+  byte low_byte  = package[PACKAGE_SIZE - 3];
+
+  return (high_byte << 8) | low_byte;
+}
+
+
+/* -- ОБРАБОТКА ИСКЛЮЧЕНИЙ -- */
+
+void handle_bad_package(PACKAGE)
+{
+  serial_reset();
+  if (package[0] !=  START_MAGIC)
+  {
+    send_package(_PE_PACKAGE_ERR, _PE_PACKAGE_ERR_MAGIC);
+  }
+  else if (
+    package[PACKAGE_SIZE - 2] != END_CR ||
+    package[PACKAGE_SIZE - 1] != END_LF)
+  {
+    send_package(_PE_PACKAGE_ERR, _PE_PACKAGE_ERR_CRLF);
+  }
+}
+
+void handle_bad_crc(PACKAGE, uint16_t crc)
+{
+  serial_reset();
+  send_package(_PE_PACKAGE_CRC, crc);
+}
+
+
+/* -- ОБРАБОТКА ЗАПРОСОВ -- */
+
+void handle_request(PACKAGE)
 {
   uint8_t cmd = package[1];
 
-  if (IN_RANGE(cmd, 0xA0, 0xAF))
+  if (cmd == _P_SERIAL_RESET)
+  {
+    serial_reset();
+  }
+  else if (IS_IN_RANGE(cmd, 0xA0, 0xAF))
   {
     handle_data_request(package);
   }
-  else if (IN_RANGE(cmd, 0xB0, 0xBF))
+  else if (IS_IN_RANGE(cmd, 0xB0, 0xBF))
   {
     handle_commands_request(package);
   }
-  else if (IN_RANGE(cmd, 0xD0, 0xDF))
+  else if (IS_IN_RANGE(cmd, 0xD0, 0xDF))
   {
     handle_commands_request(package);
   }
   else
   {
-    send_package(_P_UNKNOWN_CMD, 0);
+    send_package(_PE_UNKNOWN_CMD, 0);
   }
 }
 
-void handle_data_request(uint8_t package[])
+void handle_data_request(PACKAGE)
 {
   uint8_t cmd = package[1];
   uint32_t value = 0;
@@ -327,20 +429,20 @@ void handle_data_request(uint8_t package[])
   }
   else if (cmd == _P_REQ_SOLAR_PANELS_EF)
   {
-    uint16_t leftP = analogRead(PIN_SOLAR_PANEL_LEFT) * 100.0 / 1023.0;
-    uint16_t rightP = analogRead(PIN_SOLAR_PANEL_RIGHT) * 100.0 / 1023.0;
-    value = FLOAT_TO_INT(((leftP + rightP) / 2.0));
+    //uint16_t leftP = analogRead(PIN_SOLAR_PANEL_LEFT) * 100.0 / 1023.0;
+    //uint16_t rightP = analogRead(PIN_SOLAR_PANEL_RIGHT) * 100.0 / 1023.0;
+    value = 0; //FLOAT_TO_INT(((leftP + rightP) / 2.0));
   }
   else
   {
-    cmd = _P_UNKNOWN_CMD;
+    cmd = _PE_UNKNOWN_CMD;
     value = 0;
   }
 
   send_package(cmd, value);
 }
 
-void handle_commands_request(uint8_t package[])
+void handle_commands_request(PACKAGE)
 {
   uint8_t cmd = package[1];
   uint32_t value = 0;
@@ -356,16 +458,16 @@ void handle_commands_request(uint8_t package[])
   {
     value = pres_relief_valve_active ? 0xFF : 0x00;
   }
-  else if (cmd == _P_SWITCH_OXYGEN_SUPPLY)
+  else if (cmd == _P_SWITCH_PUMP_VALVE)
   {
-    oxygen_supply_active = !oxygen_supply_active;
+    pump_valve_active = !pump_valve_active;
 
-    digitalWrite(PIN_OXYGEN_SUPPLY, oxygen_supply_active ? LOW : HIGH);
-    value = oxygen_supply_active ? 0xFF : 0x00;
+    digitalWrite(PIN_PUMP_VALVE, pump_valve_active ? LOW : HIGH);
+    value = pump_valve_active ? 0xFF : 0x00;
   }
-  else if (cmd == _P_STATUS_OXYGEN_SUPPLY)
+  else if (cmd == _P_STATUS_PUMP_VALVE)
   {
-    value = oxygen_supply_active ? 0xFF : 0x00;
+    value = pump_valve_active ? 0xFF : 0x00;
   }
   else if (cmd == _P_SWITCH_PROD_CO2)
   {
@@ -374,20 +476,20 @@ void handle_commands_request(uint8_t package[])
     digitalWrite(PIN_PROD_CO2, prod_co2_active ? HIGH : LOW);
     value = prod_co2_active ? 0xFF : 0x00;
   }
-  else if (cmd == _P_SWITCH_PROD_CO2)
+  else if (cmd == _P_STATUS_PROD_CO2)
   {
     value = prod_co2_active ? 0xFF : 0x00;
   }
   else
   {
-    cmd = _P_UNKNOWN_CMD;
+    cmd = _PE_UNKNOWN_CMD;
     value = 0;
   }
 
   send_package(cmd, value);
 }
 
-void handle_time_request(uint8_t package[])
+void handle_time_request(PACKAGE)
 {
   uint8_t cmd = package[1];
   uint32_t value = 0;
@@ -437,7 +539,7 @@ void handle_time_request(uint8_t package[])
   }
   else
   {
-    cmd = _P_UNKNOWN_CMD;
+    cmd = _PE_UNKNOWN_CMD;
     value = 0;
   }
 
